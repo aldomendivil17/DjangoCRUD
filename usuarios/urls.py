6ba616b5ae2aaa3b468d760b6726bde432575dd0
aldomendivil17 <aldomendivil17@gmail.com>
2023-06-29 20:12:58 -0700

    Implementando un solo form para todas las funciones de usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import actualizarUser_screen, actualizarUsuario, actualizarContraseña, cambiarImagen
-
+from .views import modificarUsuario, actualizarUser_screen
 
 app_name = 'usuarios'
 
 urlpatterns  = [
+    path('modificarUsuario/', modificarUsuario, name="modificarUsuario"),
     path('actualizarUser_screen/',actualizarUser_screen , name="actualizarUser_screen"),
-    path('actualizarUsuario/', actualizarUsuario, name="actualizarUsuario"),
-    path('actualizarContraseña/', actualizarContraseña, name="actualizarContraseña"),
-    path('cambiarImagen/', cambiarImagen, name="cambiarImagen"),
+    #path('actualizarUsuario/', actualizarUsuario, name="actualizarUsuario"),
+    #path('actualizarContraseña/', actualizarContraseña, name="actualizarContraseña"),
+    #path('cambiarImagen/', cambiarImagen, name="cambiarImagen"),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -12,6 +12,58 @@ from django.contrib.auth.hashers import check_password
 def actualizarUser_screen(request):
     return render(request, 'user.html')
 
+
+@login_required
+def modificarUsuario(request):
+    if request.method == 'POST':
+        #mensajes
+        mensajes=0
+        #usuario
+        usuario = request.user
+
+        #username
+        new_username = request.POST.get('new_username') 
+
+        #imagen
+        image_file = request.FILES.get('image')
+
+        #contraseña
+        actualContraseña = request.POST.get('actualContraseña')
+        nuevaContraseña = request.POST.get('nuevaContraseña')
+        confirmacionNuevaContraseña = request.POST.get('confirmacionNuevaContraseña')
+        if new_username != "" and new_username != usuario.username:
+            usuario.username = new_username
+
+        if image_file:
+            usuario.picture = image_file
+        else:
+            print('hola')   
+        if actualContraseña and nuevaContraseña and confirmacionNuevaContraseña:
+            if check_password(actualContraseña, usuario.password):
+                if nuevaContraseña == confirmacionNuevaContraseña:
+                    if not check_password(nuevaContraseña, usuario.password):
+                        usuario.set_password(nuevaContraseña)
+                    else:
+                        messages.success(request,"La contraseña nueva es la misma que la anterior. Intenta otra vez", extra_tags='error')
+                        mensajes=1
+                else:
+                    messages.success(request,"Las contraseñas nuevas no coinciden. Intenta otra vez", extra_tags='error')
+                    mensajes=1
+            else:
+                messages.success(request,"La contraseña actual es incorrecta. Intenta otra vez", extra_tags='error')
+                mensajes=1
+        
+        usuario.save()
+        update_session_auth_hash(request, usuario)
+        if mensajes == 0:
+            messages.success(request,"El usuario '"+ usuario.username +"' se modificó correctamente", extra_tags='correcto')
+        url = reverse('usuarios:actualizarUser_screen')
+        return redirect(url)
+    
+
+
+"""
+# funciones invidivuales (no estan trajando --> se cambio por un solo form que modifique todo)
 @login_required
 def actualizarUsuario(request):
      if request.method == 'POST':
@@ -60,11 +112,10 @@ def actualizarContraseña(request):
             url = reverse('usuarios:actualizarUser_screen')
             return redirect(url)    
 
-
 @login_required
 def cambiarImagen(request):
     if request.method == 'POST':
-        image_file = request.FILES.get('image')
+        image_file = request.FILES.get('images')
         if image_file:
             print('Hola')
             request.user.picture = image_file
@@ -76,4 +127,5 @@ def cambiarImagen(request):
             messages.success(request,"No se pudo cambiar la imagen de perfil correctamente. Intenta otra vez", extra_tags='error')
             url = reverse('usuarios:actualizarUser_screen')
             return redirect(url)
-    
+# end funciones invidivuales        
+"""
